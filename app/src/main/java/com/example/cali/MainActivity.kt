4d0c014b80cf5a6841c89bd24f78e0f87ab0891a
Himanshu Yadav <himanshu24yadav@gmail.com>
2021-03-11 16:02:33 +0530

    sampleProject implemented

--- app/src/main/java/com/example/cali/MainActivity.kt
+++ app/src/main/java/com/example/cali/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.cali
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvMonthView.setOnClickListener {
+            groupBtns.visibility = View.GONE
+            monthCal.visibility = View.VISIBLE
+            tvBack.visibility = View.VISIBLE
+        }
+
+        tvDayView.setOnClickListener {
+            groupBtns.visibility = View.GONE
+            dayCal.visibility = View.VISIBLE
+            tvBack.visibility = View.VISIBLE
+        }
+
+        tvSelectionTypeView.setOnClickListener {
+            groupBtns.visibility = View.GONE
+            selectionTypeCal.visibility = View.VISIBLE
+            tvBack.visibility = View.VISIBLE
+        }
+
+        tvBack.setOnClickListener {
+            groupBtns.visibility = View.VISIBLE
+            selectionTypeCal.visibility = View.GONE
+            dayCal.visibility = View.GONE
+            monthCal.visibility = View.GONE
+            tvBack.visibility = View.GONE
+        }
     }
 }
